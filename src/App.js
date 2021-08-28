a5be96af2af4d146412a6f57db8a05384e79a9a8
Hardik Gohil <hardikgohil@Hardiks-MacBook-Pro.local>
2021-08-28 12:39:08 +0530

    update menu item

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import HomePage from './pages/homepage/homepage';
 import React from 'react';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
+
+
+const HatsPage = () => (
+  <div>
+    <h1> HATS PAGE </h1>
+  </div>
+);
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
